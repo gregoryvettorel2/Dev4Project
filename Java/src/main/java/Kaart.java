a5b4b2f61cb76ef93d474aea8cf749afd9357724
Greg <gregory.vettorel@student.ehb.be>
2021-05-23 15:29:47 +0200

    Git commited beide projecten

---
+++ Java/src/main/java/Kaart.java
@@ -0,0 +1,18 @@
+public class Kaart {
+    //Initalisiëren
+    private String intWaardeKaart;
+    public String kaartWaarde;
+
+    //Methoden
+
+    //Constructor
+    public Kaart(String waarde) {
+        this.intWaardeKaart = waarde;
+    }
+    //Getter
+    public String getKaartWaarde(){
+        return kaartWaarde;
+    }
+}
+//
+
